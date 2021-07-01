5fce54e9677a340ebb70dff0efda9db62c2e0f54
ts <ts@mobvoi.com>
2021-07-01 18:45:10 +0800

    Clean code.

---
+++ app/src/main/java/com/example/asm/travel/utils/ReflectUtil.kt
@@ -0,0 +1,32 @@
+package com.example.asm.travel.utils
+
+import com.example.asm.travel.classloader.AsmClassLoader
+import java.lang.reflect.Method
+
+object ReflectUtil {
+
+    fun loadClass(fullName: String, data: ByteArray): Class<*> {
+        val classLoader = AsmClassLoader()
+        return classLoader.defineClass(fullName, data)
+    }
+
+    fun findMethod(
+        clazz: Class<*>,
+        name: String,
+        vararg parameterTypes: Class<*>
+    ): Method {
+        return clazz.getMethod(name, *parameterTypes)
+    }
+
+    fun callMethod(
+        method: Method,
+        vararg args: Any
+    ) {
+        try {
+            method.invoke(null, *args)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+}
--- app/src/test/java/com/example/asm/travel/ExampleUnitTest.kt
+++ app/src/test/java/com/example/asm/travel/ExampleUnitTest.kt
@@ -1,36 +1,40 @@
 package com.example.asm.travel
 
-import com.example.asm.travel.classloader.AsmClassLoader
+import com.example.asm.travel.utils.ReflectUtil
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes.*
-import java.lang.Exception
 
 class ExampleUnitTest {
 
     @Test
-    fun helloWorldByAsm() {
-        val cw = ClassWriter(0)
+    fun genHelloWorldByAsm() {
+        val cw = ClassWriter(ClassWriter.COMPUTE_MAXS)
         cw.visit(V1_8, ACC_PUBLIC, "Hello", null, "java/lang/Object", null)
         val mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null)
         mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
         mv.visitLdcInsn("Hello, World")
         mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false)
         mv.visitInsn(RETURN)
-        mv.visitMaxs(2, 1)
+        mv.visitMaxs(1, 1)
         mv.visitEnd()
         cw.visitEnd()
 
-        val classLoader = AsmClassLoader()
-        val clazz = classLoader.defineClass("Hello", cw.toByteArray())
-        try {
-            val main = clazz.getMethod("main", Array<String>::class.java)
-            main.invoke(null, arrayOf<String>())
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        ReflectUtil.callMethod(
+            ReflectUtil.findMethod(
+                ReflectUtil.loadClass("Hello", cw.toByteArray()),
+                "main",
+                Array<String>::class.java
+            ),
+            arrayOf<String>()
+        )
     }
 
+    @Test
+    fun genSubclassByAsm() {
+        val cw = ClassWriter(0)
+        cw.visit(V1_8, ACC_PUBLIC, "Hello", null, "java/lang/Object", null)
+    }
 }
