9a922478bd506c36fcb6e1c6e0c0dda49cf8c504
ts <ts@mobvoi.com>
2021-07-03 23:17:57 +0800

    Add code to generate binary search code.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 使用`Transform`及`ASM`的一些简单实践
 
-## 生成新类
+## 一些常用的操作
+
+### 生成新类
 ```
 val cw = ClassWriter(ClassWriter.COMPUTE_MAXS)
 cw.visit(V1_8, ACC_PUBLIC, "Hello", null, "java/lang/Object", null)
@@ -16,7 +18,7 @@ mv.visitEnd()
 cw.visitEnd()
 ```
 
-## 给某个类添加新的方法
+### 给某个类添加新的方法
 ```
 val classReader = ClassReader(FileInputStream(file))
 val classWriter = CallerClassWriter(ClassWriter(ClassWriter.COMPUTE_MAXS))
@@ -45,7 +47,7 @@ methodVisitor.visitEnd()
 classWriter.visitEnd()
 ```
 
-## 删除类的某个方法
+### 删除类的某个方法
 
 visitMethod的时候判断方法名，如果是要删除的方法，返回`null`即可
 
@@ -65,7 +67,7 @@ override fun visitMethod(
 
 ```
 
-## 修改类的某个方法
+### 修改类的某个方法
 
 ```
 override fun visitCode() {
@@ -107,8 +109,87 @@ override fun visitInsn(opcode: Int) {
 }
 ```
 
+### 生成二分查找代码
+
+```
+mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "binarySearch", "([II)I", null, null)
+mv.visitCode()
+mv.visitVarInsn(ALOAD, 0)
+val l0 = Label()
+mv.visitJumpInsn(IFNULL, l0)
+mv.visitVarInsn(ALOAD, 0)
+mv.visitInsn(ARRAYLENGTH)
+val l1 = Label()
+mv.visitJumpInsn(IFNE, l1)
+mv.visitLabel(l0)
+mv.visitInsn(ICONST_M1)
+mv.visitInsn(IRETURN)
+mv.visitLabel(l1)
+mv.visitInsn(ICONST_0)
+mv.visitVarInsn(ISTORE, 2)
+mv.visitVarInsn(ALOAD, 0)
+mv.visitInsn(ARRAYLENGTH)
+mv.visitInsn(ICONST_1)
+mv.visitInsn(ISUB)
+mv.visitVarInsn(ISTORE, 3)
+val l2 = Label()
+mv.visitLabel(l2)
+mv.visitVarInsn(ILOAD, 2)
+mv.visitVarInsn(ILOAD, 3)
+val l3 = Label()
+mv.visitJumpInsn(IF_ICMPGT, l3)
+mv.visitVarInsn(ILOAD, 2)
+mv.visitVarInsn(ILOAD, 3)
+mv.visitVarInsn(ILOAD, 2)
+mv.visitInsn(ISUB)
+mv.visitInsn(ICONST_2)
+mv.visitInsn(IDIV)
+mv.visitInsn(IADD)
+mv.visitVarInsn(ISTORE, 4)
+mv.visitVarInsn(ALOAD, 0)
+mv.visitVarInsn(ILOAD, 4)
+mv.visitInsn(IALOAD)
+mv.visitVarInsn(ILOAD, 1)
+val l4 = Label()
+mv.visitJumpInsn(IF_ICMPNE, l4)
+mv.visitVarInsn(ILOAD, 4)
+mv.visitInsn(IRETURN)
+mv.visitLabel(l4)
+mv.visitVarInsn(ALOAD, 0)
+mv.visitVarInsn(ILOAD, 4)
+mv.visitInsn(IALOAD)
+mv.visitVarInsn(ILOAD, 1)
+val l5 = Label()
+mv.visitJumpInsn(IF_ICMPGE, l5)
+mv.visitVarInsn(ILOAD, 4)
+mv.visitInsn(ICONST_1)
+mv.visitInsn(IADD)
+mv.visitVarInsn(ISTORE, 2)
+val l6 = Label()
+mv.visitJumpInsn(GOTO, l6)
+mv.visitLabel(l5)
+mv.visitVarInsn(ILOAD, 4)
+mv.visitInsn(ICONST_1)
+mv.visitInsn(ISUB)
+mv.visitVarInsn(ISTORE, 3)
+mv.visitLabel(l6)
+mv.visitJumpInsn(GOTO, l2)
+mv.visitLabel(l3)
+mv.visitInsn(ICONST_M1)
+mv.visitInsn(IRETURN)
+mv.visitMaxs(3, 5)
+mv.visitEnd()
+
+cw.visitEnd()
+
+```
+
 
 ## FAQ
+
+### 不熟悉字节码指令怎么办
+
+推荐一款`Android Studio`上可用的插件[ASM Bytecode Viewer Support Kotlin](https://plugins.jetbrains.com/plugin/14860-asm-bytecode-viewer-support-kotlin)，直接展示源代码对应的字节码，可用来参考。
 
 ### 添加transform之后apk无法安装，`INSTALL_FAILED_INVALID_APK`
 
@@ -128,3 +209,7 @@ input.jarInputs.forEach { jarInput ->
 
 ```
 
+### 生成代码的指令报错，不太清楚如何排查怎么办
+
+可以使用`CheckClassAdapter`这个类来包装`ClassWriter`，有一些易懂的报错信息。
+其他的一些实用的工具类也可以研究一下，例如`AnalyzerAdapter`, `LocalVariablesSorter`, `AdviceAdapter`, `TraceClassVisitor`等。
--- app/src/test/java/com/example/asm/travel/ExampleUnitTest.kt
+++ app/src/test/java/com/example/asm/travel/ExampleUnitTest.kt
@@ -5,7 +5,9 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Label
 import org.objectweb.asm.Opcodes.*
+import org.objectweb.asm.util.CheckClassAdapter
 
 class ExampleUnitTest {
 
@@ -32,4 +34,129 @@ class ExampleUnitTest {
         )
     }
 
+    @Test
+    fun genBinarySearchByAsm() {
+        val writer = ClassWriter(ClassWriter.COMPUTE_FRAMES)
+        val cw = CheckClassAdapter(writer)
+
+        cw.visit(V1_8, ACC_PUBLIC, "Hello", null, "java/lang/Object", null)
+
+        var mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null)
+        mv.visitCode()
+        mv.visitInsn(ICONST_5)
+        mv.visitIntInsn(NEWARRAY, T_INT)
+        mv.visitInsn(DUP)
+        mv.visitInsn(ICONST_0)
+        mv.visitInsn(ICONST_1)
+        mv.visitInsn(IASTORE)
+        mv.visitInsn(DUP)
+        mv.visitInsn(ICONST_1)
+        mv.visitInsn(ICONST_2)
+        mv.visitInsn(IASTORE)
+        mv.visitInsn(DUP)
+        mv.visitInsn(ICONST_2)
+        mv.visitInsn(ICONST_3)
+        mv.visitInsn(IASTORE)
+        mv.visitInsn(DUP)
+        mv.visitInsn(ICONST_3)
+        mv.visitInsn(ICONST_5)
+        mv.visitInsn(IASTORE)
+        mv.visitInsn(DUP)
+        mv.visitInsn(ICONST_4)
+        mv.visitIntInsn(BIPUSH, 6)
+        mv.visitInsn(IASTORE)
+        mv.visitVarInsn(ASTORE, 1)
+        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+        mv.visitVarInsn(ALOAD, 1)
+        mv.visitInsn(ICONST_2)
+        mv.visitMethodInsn(INVOKESTATIC, "com/example/asm/travel/target/Test", "binarySearch", "([II)I", false)
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false)
+        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+        mv.visitVarInsn(ALOAD, 1)
+        mv.visitInsn(ICONST_4)
+        mv.visitMethodInsn(INVOKESTATIC, "com/example/asm/travel/target/Test", "binarySearch", "([II)I", false)
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false)
+        mv.visitInsn(RETURN)
+        mv.visitMaxs(4, 2)
+        mv.visitEnd()
+
+        mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "binarySearch", "([II)I", null, null)
+        mv.visitCode()
+        mv.visitVarInsn(ALOAD, 0)
+        val l0 = Label()
+        mv.visitJumpInsn(IFNULL, l0)
+        mv.visitVarInsn(ALOAD, 0)
+        mv.visitInsn(ARRAYLENGTH)
+        val l1 = Label()
+        mv.visitJumpInsn(IFNE, l1)
+        mv.visitLabel(l0)
+        mv.visitInsn(ICONST_M1)
+        mv.visitInsn(IRETURN)
+        mv.visitLabel(l1)
+        mv.visitInsn(ICONST_0)
+        mv.visitVarInsn(ISTORE, 2)
+        mv.visitVarInsn(ALOAD, 0)
+        mv.visitInsn(ARRAYLENGTH)
+        mv.visitInsn(ICONST_1)
+        mv.visitInsn(ISUB)
+        mv.visitVarInsn(ISTORE, 3)
+        val l2 = Label()
+        mv.visitLabel(l2)
+        mv.visitVarInsn(ILOAD, 2)
+        mv.visitVarInsn(ILOAD, 3)
+        val l3 = Label()
+        mv.visitJumpInsn(IF_ICMPGT, l3)
+        mv.visitVarInsn(ILOAD, 2)
+        mv.visitVarInsn(ILOAD, 3)
+        mv.visitVarInsn(ILOAD, 2)
+        mv.visitInsn(ISUB)
+        mv.visitInsn(ICONST_2)
+        mv.visitInsn(IDIV)
+        mv.visitInsn(IADD)
+        mv.visitVarInsn(ISTORE, 4)
+        mv.visitVarInsn(ALOAD, 0)
+        mv.visitVarInsn(ILOAD, 4)
+        mv.visitInsn(IALOAD)
+        mv.visitVarInsn(ILOAD, 1)
+        val l4 = Label()
+        mv.visitJumpInsn(IF_ICMPNE, l4)
+        mv.visitVarInsn(ILOAD, 4)
+        mv.visitInsn(IRETURN)
+        mv.visitLabel(l4)
+        mv.visitVarInsn(ALOAD, 0)
+        mv.visitVarInsn(ILOAD, 4)
+        mv.visitInsn(IALOAD)
+        mv.visitVarInsn(ILOAD, 1)
+        val l5 = Label()
+        mv.visitJumpInsn(IF_ICMPGE, l5)
+        mv.visitVarInsn(ILOAD, 4)
+        mv.visitInsn(ICONST_1)
+        mv.visitInsn(IADD)
+        mv.visitVarInsn(ISTORE, 2)
+        val l6 = Label()
+        mv.visitJumpInsn(GOTO, l6)
+        mv.visitLabel(l5)
+        mv.visitVarInsn(ILOAD, 4)
+        mv.visitInsn(ICONST_1)
+        mv.visitInsn(ISUB)
+        mv.visitVarInsn(ISTORE, 3)
+        mv.visitLabel(l6)
+        mv.visitJumpInsn(GOTO, l2)
+        mv.visitLabel(l3)
+        mv.visitInsn(ICONST_M1)
+        mv.visitInsn(IRETURN)
+        mv.visitMaxs(3, 5)
+        mv.visitEnd()
+
+        cw.visitEnd()
+
+        ReflectUtil.callStaticMethod(
+            ReflectUtil.findMethod(
+                ReflectUtil.loadClass("Hello", writer.toByteArray()),
+                "main",
+                Array<String>::class.java
+            ),
+            arrayOf<String>()
+        )
+    }
 }
